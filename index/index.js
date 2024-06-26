d3008125e170235da9f11f79e8b02ff2e2b4d71c
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-06-26 20:50:45 +0900

    Update index.js

--- index/index.js
+++ index/index.js
@@ -1,4 +1,11 @@
 function question1() {
-  var value = document.getElementById("field").value
-  if(value === )//a=??,b=??,c=??,d=??
+  var value1 = document.getElementById("fieldA").value
+  var value2 = document.getElementById("fieldB").value
+  var value3 = document.getElementById("fieldC").value
+  if(value1 === 2 && value2 === 3 && value3 === 15){
+    window.location.href = "question2.html"
+  }else {
+    document.getElementById("answer").style.display = "none"
+    document.getElementById("misstake").style.diplay = "block"
+  }
 }
