d356cc588766d27c1ba226228b6c4dd1aa651252
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-06-26 22:00:48 +0900

    Update index.js

--- index/index.js
+++ index/index.js
@@ -2,11 +2,12 @@ function question1() {
   var value1 = document.getElementById("fieldA").value
   var value2 = document.getElementById("fieldB").value
   var value3 = document.getElementById("fieldC").value
-  if(value1 === 2 && value2 === 3 && value3 === 15){
+  alert(value1 + "," + value2 + "," + value3)
+  if(value1 == 2 && value2 == 3 && value3 == 15){
     window.location.href = "question2.html"
   }else {
     document.getElementById("answer").style.display = "none"
-    document.getElementById("misstake").style.diplay = "block"
+    document.getElementById("misstake").style.display = "block"
   }
 }
 
