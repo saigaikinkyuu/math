1c52aef75f5f30db089fa9d66011e4294dc71535
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-06-28 20:23:43 +0900

    Create load.js

---
+++ index/load.js
@@ -0,0 +1,7 @@
+$.getJSON("https://script.google.com/macros/s/AKfycbyD8llwZGIAZ8Dqvs7uv0LgHjDR45oCj95OvP8LLzLCkg0JEeXWElrS9ex7GutPlB70/exec", function (data) {
+  if(data[0]){
+    if(data[0].value === "Yes"){
+      window.location.href = "./close.html"
+    }
+  }
+})
