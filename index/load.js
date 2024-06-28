73b23cae265b916e3e834b2384cbcdb5846ad775
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-06-28 20:42:40 +0900

    Update load.js

--- index/load.js
+++ index/load.js
@@ -1,10 +1,14 @@
+let interval = null
 document.getElementById('body').style.display = "none"
-$.getJSON("https://script.google.com/macros/s/AKfycbyD8llwZGIAZ8Dqvs7uv0LgHjDR45oCj95OvP8LLzLCkg0JEeXWElrS9ex7GutPlB70/exec", function (data) {
-  if(data[0]){
-    if(data[0].value === "Yes"){
-      window.location.href = "./close.html"
-    }else {
-      document.getElementById('body').style.display = "block"
+function checkAccess() {
+  $.getJSON("https://script.google.com/macros/s/AKfycbyD8llwZGIAZ8Dqvs7uv0LgHjDR45oCj95OvP8LLzLCkg0JEeXWElrS9ex7GutPlB70/exec", function (data) {
+    if(data[0]){
+      if(data[0].value === "Yes"){
+        window.location.href = "./close.html"
+      }else {
+        document.getElementById('body').style.display = "block"
+      }
     }
-  }
-})
+  })
+}
+interval = setInterval(checkAccess, 1000);
