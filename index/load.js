46fba5413ed7294152c637401dc3b2124c06d016
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-01 17:34:00 +0900

    Update load.js

--- index/load.js
+++ index/load.js
@@ -1,14 +1,20 @@
 let interval = null
-document.getElementById('body').style.display = "none"
-function checkAccess() {
+function checkAccessFirst() {
   $.getJSON("https://script.google.com/macros/s/AKfycbyD8llwZGIAZ8Dqvs7uv0LgHjDR45oCj95OvP8LLzLCkg0JEeXWElrS9ex7GutPlB70/exec", function (data) {
     if(data[0]){
-      if(data[0].value === "Yes"){
+      if(data[0].value === "No"){
         window.location.href = "./close.html"
       }else {
-        document.getElementById('body').style.display = "block"
+        // URLのクエリ文字列を取得
+        const queryString = window.location.search;
+        // URLSearchParamsオブジェクトを作成してクエリ文字列を解析
+        const params = new URLSearchParams(queryString);
+        // 特定のパラメータの値を取得
+        const page = params.get('pages');
+        const d = params.get('d');
+        window.location.href = "./" + d + "/" + pages + ".html"
       }
     }
   })
 }
-interval = setInterval(checkAccess, 1000);
+interval = setInterval(checkAccessFirst, 1000);
