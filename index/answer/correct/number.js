6ce7fa0c053254e86ab2f29d388104d53991ab20
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-01 20:18:33 +0900

    Create number.js

---
+++ index/answer/correct/number.js
@@ -0,0 +1,11 @@
+// URLのクエリ文字列を取得
+const queryString = window.location.search;
+// URLSearchParamsオブジェクトを作成してクエリ文字列を解析
+const params = new URLSearchParams(queryString);
+// 特定のパラメータの値を取得
+const answer = params.get('answer');
+if(answer == "23158411"){
+  document.getElementById('lock').innerHTML = "4725"
+}else if(answer == "02505075144"){
+  document.getElementById('lock').innerHTML = "8437"
+}
