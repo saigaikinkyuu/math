b08a7cdde14307b28f2c2b91c36ca85b1a72a083
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-01 20:44:08 +0900

    Create checkLicense.js

---
+++ index/answer/correct/checkLicense.js
@@ -0,0 +1,33 @@
+function checkAccesslicense() {
+  // URLのクエリ文字列を取得
+  const queryString = window.location.search;
+  // URLSearchParamsオブジェクトを作成してクエリ文字列を解析
+  const params = new URLSearchParams(queryString);
+  // 特定のパラメータの値を取得
+  const l = params.get('l');
+  $.getJSON("https://script.google.com/macros/s/AKfycbwb6TXn3XZlVLLEq4Rvhy-HE7CGqIBzdVxEMnxPSpMf4FnfvNgi7uM5tlnqSxKOvT8v/exec", function (data) {
+    console.log(data.length)
+    if(data[0]){
+      for(var i = 0;i<data.length;i++){
+        console.log(data[i].ID)
+        if(data[i].ID !== l && (i+2)>data.length){
+          window.location.href = "../../error.html"
+        }else if(data[i].ID === l){
+          i = data.length+1
+        }
+      }
+    }else {
+      window.location.href = "../../error.html"
+    }
+  })
+  $.getJSON("https://script.google.com/macros/s/AKfycbyD8llwZGIAZ8Dqvs7uv0LgHjDR45oCj95OvP8LLzLCkg0JEeXWElrS9ex7GutPlB70/exec", function (data) {
+    if(data[0]){
+      if(data[0].value !== "No"){//実際はノットイコール
+        window.location.href = "../../close.html"
+      }
+    }else {
+      window.location.href = "../../error.html"
+    }
+  })
+}
+checkAccesslicense()
