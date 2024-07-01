2a138132acb4e5da29142e82cfc795b071b8b7c2
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-01 19:11:13 +0900

    Update loadClose.js

--- index/loadClose.js
+++ index/loadClose.js
@@ -3,7 +3,7 @@ function checkAccess() {
   $.getJSON("https://script.google.com/macros/s/AKfycbyD8llwZGIAZ8Dqvs7uv0LgHjDR45oCj95OvP8LLzLCkg0JEeXWElrS9ex7GutPlB70/exec", function (data) {
     if(data[0]){
       if(data[0].value === "No"){
-        window.location.href = "./question1.html"
+        window.location.href = "./access.html?d=0702&pages=question1"
       }
     }
   })
