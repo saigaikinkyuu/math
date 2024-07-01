b9862117b974781725c00011ea5026aefcbad3af
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-01 21:19:37 +0900

    Update checkLicense.js

--- index/answer/correct/checkLicense.js
+++ index/answer/correct/checkLicense.js
@@ -14,6 +14,15 @@ function checkAccesslicense() {
           window.location.href = "../../error.html"
         }else if(data[i].ID === l){
           i = data.length+1
+          // 特定のパラメータの値を取得
+          const answer = params.get('answer');
+          if(answer == "23158411"){
+            document.getElementById('lock').innerHTML = "4725"
+          }else if(answer == "02505075144"){
+            document.getElementById('lock').innerHTML = "8437"
+          }else {
+            window.location.href = "../../error.html"
+          }
         }
       }
     }else {
@@ -29,14 +38,5 @@ function checkAccesslicense() {
       window.location.href = "../../error.html"
     }
   })
-  // 特定のパラメータの値を取得
-  const answer = params.get('answer');
-  if(answer == "23158411"){
-    document.getElementById('lock').innerHTML = "4725"
-  }else if(answer == "02505075144"){
-    document.getElementById('lock').innerHTML = "8437"
-  }else {
-    window.location.href = "../../error.html"
-  }
 }
 checkAccesslicense()
