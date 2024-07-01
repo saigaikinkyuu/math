27e6416779bcccdb93a52c68f735cc5fc865556b
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-01 19:11:35 +0900

    Update checkLicense.js

--- index/checkLicense.js
+++ index/checkLicense.js
@@ -19,7 +19,7 @@ function checkAccesslicense() {
   })
   $.getJSON("https://script.google.com/macros/s/AKfycbyD8llwZGIAZ8Dqvs7uv0LgHjDR45oCj95OvP8LLzLCkg0JEeXWElrS9ex7GutPlB70/exec", function (data) {
     if(data[0]){
-      if(data[0].value === "No"){//実際はノットイコール
+      if(data[0].value !== "No"){//実際はノットイコール
         window.location.href = "./close.html"
       }
     }else {
