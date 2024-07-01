9cec9c3f51e99fc16c0c88695ef829777d2ffda5
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-01 20:46:31 +0900

    Update checkLicense.js

--- index/answer/correct/checkLicense.js
+++ index/answer/correct/checkLicense.js
@@ -29,5 +29,14 @@ function checkAccesslicense() {
       window.location.href = "../../error.html"
     }
   })
+  // 特定のパラメータの値を取得
+  const answer = params.get('answer');
+  if(answer == "23158411"){
+    document.getElementById('lock').innerHTML = "4725"
+  }else if(answer == "02505075144"){
+    document.getElementById('lock').innerHTML = "8437"
+  }else {
+    window.location.href = "../../error.html"
+  }
 }
 checkAccesslicense()
